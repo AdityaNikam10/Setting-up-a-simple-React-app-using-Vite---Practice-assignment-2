eedd6cf8a28fca7eadab8498215eed60deb40592
Aditya Nikam <aditya.nikam.s73@kalvium.community>
2025-04-28 09:49:29 +0530

    completed

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Aditya Nikam</h1>
+            <p>Contact: aditya@example.com</p>
+        </div>
+    );
+};
+
+export default Home;
